31f9a27b0468bfc1a4986aae5ed513df527efb94
mahaagromart <mahaagromart.it.1965@gmail.com>
2025-02-14 11:55:34 +0530

    Update AuthenticationController.cs

--- ECOMAPP/Controllers/AuthenticationController.cs
+++ ECOMAPP/Controllers/AuthenticationController.cs
@@ -60,7 +60,7 @@ namespace ECOMAPP.Controllers
             }
 
             public List<AuthenticationDTO.UserProfileEntites> UserProfilesEntity { get; set; }
-            public List<AuthenticationDTO.AuthenticationEntites> AuthenticationsList { get; set; }
+            public List<AuthenticationDTO.AuthenticationEntites> AuthenticationsList { get; geet; }
 
         }
 
