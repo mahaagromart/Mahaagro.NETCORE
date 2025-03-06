83984d037281c10695fba55717b0a1b4032465fd
mahaagromart <mahaagromart.it.1965@gmail.com>
2025-03-06 13:08:59 +0530

    Update DBEnums.cs
    added more status codes

--- ECOMAPP/CommonRepository/DBEnums.cs
+++ ECOMAPP/CommonRepository/DBEnums.cs
@@ -8,7 +8,22 @@
             ADD = 0, UPDATE = 1, DELETE = 2, UNDELETE = 3, SEARCH = 4, VIEW = 5, COPY = 6
         }
 
-        
+        public enum Codes
+        {
+            Success = 200,
+            Created = 201,
+            Accepted = 202,
+            NoContent = 204,
+            BadRequest = 400,
+            Unauthorized = 401,
+            Forbidden = 403,
+            NotFound = 404,
+            Conflict = 409,
+            InternalServerError = 500,
+            NotImplemented = 501,
+            BadGateway = 502,
+            ServiceUnavailable = 503
+        }
         //public enum Code { s, d }
 
 
